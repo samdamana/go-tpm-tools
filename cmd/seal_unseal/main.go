7a8b613a0ed37a9c09a0fb9031c1e3b12440506e
Samuel Davidson <samueldavidson@google.com>
2019-02-04 13:43:11 -0800

    Added srkHandle creation.

--- cmd/seal_unseal/main.go
+++ cmd/seal_unseal/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/hex"
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/google/go-tpm/tpm2"
 )
@@ -12,8 +13,7 @@ var (
 	secretMessage = "SamIsMySpecialGuy"
 	tpmPath       = flag.String("tpm-path", "/dev/tpm0", "path to a TPM character device or socket")
 	pcr           = flag.Int("pcr", 7, "PCR to seal data to. Must be within [0, 23].")
-	srkHandle     = tpm2.HandleNull // TODO
-	srkPassword   = ""              // TODO
+	srkPassword   = "" // TODO
 	srkTemplate   = tpm2.Public{
 		Type:       tpm2.AlgRSA,
 		NameAlg:    tpm2.AlgSHA256,
@@ -33,7 +33,45 @@ var (
 )
 
 func main() {
+	flag.Parse()
+
+	if *pcr < 0 || *pcr > 23 {
+		fmt.Fprintf(os.Stderr, "Invalid flag 'pcr': value %d is out of range", *pcr)
+		os.Exit(1)
+	}
+
+	err := run(*pcr, *tpmPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(pcr int, tpmPath string) (retErr error) {
 	fmt.Println("Testing Seal/Unseal vTPM stuff.")
 	fmt.Printf("Secret Str: \"%s\"\nSecret Hex: %s\n", secretMessage, hex.EncodeToString([]byte(secretMessage)))
+	// Open the TPM
+	rwc, err := tpm2.OpenTPM(tpmPath)
+	if err != nil {
+		return fmt.Errorf("can't open TPM %q: %v", tpmPath, err)
+	}
+	defer func() {
+		if err := rwc.Close(); err != nil {
+			retErr = fmt.Errorf("%v\ncan't close TPM %q: %v", retErr, tpmPath, err)
+		}
+	}()
 
+	srkHandle, publicKey, err := tpm2.CreatePrimary(rwc, tpm2.HandleOwner, tpm2.PCRSelection{}, "", srkPassword, srkTemplate)
+	if err != nil {
+		return fmt.Errorf("can't create primary key: %v", err)
+	}
+	defer func() {
+		if err := tpm2.FlushContext(rwc, srkHandle); err != nil {
+			retErr = fmt.Errorf("%v\nunable to flush SRK handle %q: %v", retErr, srkHandle, err)
+		}
+	}()
+
+	fmt.Printf("srkHandle: 0x%x\npublicKey: %v\n", srkHandle, publicKey)
+
+	return nil
 }
